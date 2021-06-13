433452a6b5a47c467d827917aefc96320b12ab8d
Alexandra Shmytko <kitsamura@MacBook-Air-Alexandra.local>
2021-06-14 01:53:48 +0300

    children support

---
+++ src/components/ModalSelect1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Multiselect v-model="value" placeholder="Выберете начальника" :options="elems"
+      @input="clickOption" @click="clicker"/>
+  </div>
+</template>
+
+<script>
+  import Multiselect from '@vueform/multiselect'
+
+  export default {
+    props: ['elems'],
+    components: {
+      Multiselect
+    },
+    data() {
+      return {
+        type: Array,
+        value: null,
+      }
+    },
+    methods: {
+      clickOption(value) {
+        // console.log(value);
+        this.$emit('addChief', value)
+      },
+      clicker(){
+        console.log(this.options);
+      }
+    }
+  }
+</script>
+
+<style src="@vueform/multiselect/themes/default.css"></style>
